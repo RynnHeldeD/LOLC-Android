d375e191dbe510bc693d917a9eff9a3dd537c45a
Sogeki-hei Michiru <michel.parreno@mines-ales.org>
2015-06-03 18:57:17 +0200

    Ajout des fonctionnalites allies et ennemies    * Les deux vues sont creer et les ressources ont leur id definitifs    * Une fonction specifique a ete creer sur une acitivite parente pour remplir facilement toutes les infos des summoners    * les images des rang et des vues sont ajoutees    * Les ressources sont à jours pour permettre de changer facilement polices et dimensions    - Il manque le script pour les tooltip champion

--- app/src/main/java/org/ema/lolcompanion/AlliesActivity.java
+++ app/src/main/java/org/ema/lolcompanion/AlliesActivity.java
@@ -1,24 +1,47 @@
 package org.ema.lolcompanion;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.ema.model.business.Summoner;
+import org.ema.utils.LoLStatActivity;
+import org.ema.utils.VerticalProgressBar;
 
-public class AlliesActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class AlliesActivity extends LoLStatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allies);
 
+        //setting the font for title
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/lol.ttf");
         TextView allies = (TextView) findViewById(R.id.allies);
         allies.setTypeface(font);
+
+        //keeping screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //TO-DO - Foreach ListSummoner - Do FillSummonerInfo
     }
 
 }
--- app/src/main/java/org/ema/lolcompanion/EnnemiesActivity.java
+++ app/src/main/java/org/ema/lolcompanion/EnnemiesActivity.java
@@ -6,10 +6,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.TextView;
 
+import org.ema.utils.LoLStatActivity;
 
-public class EnnemiesActivity extends Activity {
+
+public class EnnemiesActivity extends LoLStatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,9 @@ public class EnnemiesActivity extends Activity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/lol.ttf");
         TextView ennemies = (TextView) findViewById(R.id.ennemies);
         ennemies.setTypeface(font);
+
+        //keeping screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //TO-DO - Foreach ListSummoner - Do FillSummonerInfo
     }
 }

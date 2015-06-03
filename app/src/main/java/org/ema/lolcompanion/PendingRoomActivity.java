51d85314a9a82295b12e333caf6f1242d7da5131
Constantin <veltri.constantin@gmail.com>
2015-06-03 11:34:49 +0300

    Le pseudo est recuperer de la premiere vue pour aller dans la pending room. Si le pseudo a des caracteres interdits, on ne fait rien. TODO : mettre une pop up d'erreur. TODO: changer le texte 'Waiting game signal' en 'Loading your data' dans la pending room quand on detecte que la personne est en game.

--- app/src/main/java/org/ema/lolcompanion/PendingRoomActivity.java
+++ app/src/main/java/org/ema/lolcompanion/PendingRoomActivity.java
@@ -3,15 +3,30 @@ package org.ema.lolcompanion;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import android.util.Log;
+import org.ema.model.DAO.CurrentGameDAO;
+import org.ema.model.DAO.SummonerDAO;
+import org.ema.model.business.Summoner;
+import java.util.ArrayList;
 
 public class PendingRoomActivity extends Activity {
+
+    public ArrayList<Summoner> summonerList;
+    public Thread waitingThread;
+    //public Handler handlerWaitingThread;
+    public int count = 0;
+    public boolean shouldContinue = true;
+    public Summoner user;
+    public String summonerNameFromPreviousView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +39,7 @@ public class PendingRoomActivity extends Activity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/lol.ttf");
 
         //setting summoner name
-        String summonerNameFromPreviousView = intent.getStringExtra(MainActivity.SUMMONER_NAME);
+        summonerNameFromPreviousView = intent.getStringExtra(MainActivity.SUMMONER_NAME);
         TextView summonerName = (TextView) findViewById(R.id.pending_summoner_name);
         summonerName.setTypeface(font);
         summonerName.setText(summonerNameFromPreviousView);
@@ -34,11 +49,72 @@ public class PendingRoomActivity extends Activity {
         pending.setTypeface(font);
         TextView pendingDescription = (TextView) findViewById(R.id.pending_description);
         pendingDescription.setTypeface(font);
-
         //to set the loading off : findViewById(R.id.loadin_panel).setVisibility(View.GONE);
 
+        //Launch pending task
+        user = SummonerDAO.getSummoner(summonerNameFromPreviousView);
 
+        //Creating thread
+        waitingThread = new Thread(new Runnable() {
+            public void run() {
+                while(shouldContinue) {
+                    Log.v("Thread", "New thread running");
+                    //Waiting 10 seconds before make a new request to the server
+                    if(!loadData()){
+                        SystemClock.sleep(10000);
+                    }
+                }
+                Log.v("DAO", "On a recupere les , on affiche la vue timer");
+                launchTimerActivity();
+            }
+        });
+
+        //Launch thread if the user login is good
+        if (user != null) {
+            Log.v("DAO", user.toString());
+            waitingThread.start();
+        }
     }
 
+    public void launchTimerActivity() {
+        Intent intent = new Intent(this, TimerActivity.class);
+        //TODO : mettre le channel enregistre dans la case channel
+        // MainActivity.settingsManager.set(this, "summonerName", message);
+        startActivity(intent);
+    }
+
+    public boolean loadData() {
+
+        count ++;
+        if(count == 10)
+        {
+            Log.v("Error", "Interrupted");
+            waitingThread.interrupt();
+        }
+        int id = user.getId();
+        boolean isInGame = SummonerDAO.isInGame(id);
+        Log.v("DAO", "Is in game: " + isInGame);
+
+        if (isInGame) {
+            //TODO : impossible d'acceder aux ressources via un thread, voir comment recuperer et setter la ressource
+            //On change le texte "Waiting signal game" en "Loading your data"
+            TextView pendingRoomText = (TextView) findViewById(R.id.pending_initial_state);
+            //pendingRoomText.setText(this.getString(R.string.pending_loading_state));
+            //Fin TODO
+
+            pendingRoomText.setText("Loading your data...");
+
+            summonerList = CurrentGameDAO.getSummunerListInGameFromCurrentUser(user);
+            if (summonerList != null) {
+                Log.v("DAO", "SummonerList: " + summonerList.toString());
+            }
+            shouldContinue = false;
+            return true;
+        } else {
+            Log.v("Error", "User not in game");
+            shouldContinue = true;
+        }
+        return false;
+    }
 
 }

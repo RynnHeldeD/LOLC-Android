1234d382bd26cd942c77b180fa8e209c09be1f4c
Romain <galli.romain24@gmail.com>
2015-06-04 12:33:20 +0200

    GetRegions.

---
+++ app/src/main/java/org/ema/utils/Region.java
@@ -0,0 +1,25 @@
+package org.ema.utils;
+
+/**
+ * Created by romain on 04/06/2015.
+ */
+public enum Region {
+    //North America
+    NA,
+    //Europe West
+    EUW,
+    //Europe Nordic & East
+    EUNE,
+    //Brazil
+    BR,
+    //Turkey
+    TR,
+    //Russia
+    RU,
+    //Latin America North
+    LAN,
+    //Latin America South
+    LAS,
+    //Oceania
+    OCE
+}

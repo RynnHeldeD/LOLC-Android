f93584cf8f0e0a77f51bce2b5caaa83603d4a214
Constantin Veltri <veltri.constantin@gmail.com>
2015-11-19 19:08:19 +0100

    Changement du texte copyright pour enlever les caracteres speciaux

--- app/src/main/java/org/ema/model/business/Item.java
+++ app/src/main/java/org/ema/model/business/Item.java
@@ -1,19 +1,19 @@
-/* Copyright © 2015
+/* Copyright  2015
  * GHARBI Eddy
  * PARRENO Michel
  * VELTRI Constantin
  * NGUYEN Remy
  * GALLI Romain
  *
- * Cette œuvre est protégée par le droit d’auteur et strictement réservée à l’usage privé du
- * client. Toute reproduction ou diffusion au profit de tiers, à titre
- * gratuit ou onéreux, de
- * tout ou partie de cette œuvre est strictement interdite et constitue une contrefaçon prévue
- * par les articles L 335-2 et suivants du Code de la propriété
+ * Cette oeuvre est protegee par le droit d auteur et strictement reservee a l usage prive du
+ * client. Toute reproduction ou diffusion au profit de tiers, a titre
+ * gratuit ou onereux, de
+ * tout ou partie de cette oeuvre est strictement interdite et constitue une contrefacon prevue
+ * par les articles L 335-2 et suivants du Code de la propriete
  * intellectuelle. Les ayants-droits se
- * réservent le droit de poursuivre toute atteinte à leurs droits de
- * propriété intellectuelle devant les
- * juridictions civiles ou pénales.
+ * reservent le droit de poursuivre toute atteinte a leurs droits de
+ * propriete intellectuelle devant les
+ * juridictions civiles ou penales.
  */
 
 package org.ema.model.business;
